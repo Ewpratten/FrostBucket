9a65fbe992f075219ad3551bca8960d3608a7a02
Evan Pratten <ewpratten@gmail.com>
2021-01-19 10:19:57 -0500

    skele of source

---
+++ src/main/java/ca/retrylife/FrostBucket/FrostBucketPlugin.java
@@ -0,0 +1,34 @@
+package ca.retrylife.FrostBucket;
+
+import kr.entree.spigradle.annotations.PluginMain;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPluginLoader;
+
+import java.io.File;
+
+@PluginMain
+public class FrostBucketPlugin extends JavaPlugin implements Listener {
+
+    public FrostBucketPlugin() {}
+
+    public FrostBucketPlugin(
+        JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file) {
+        super(loader, description, dataFolder, file);
+    }
+
+    @Override
+    public void onEnable() {
+
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        getLogger().info("Player joined.");
+    }
+    
+}
